dfa17cc1e51127eba7310f72432322e4d1f4bc03
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-09-10 21:05:24 -0400

    add 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100 + 1):
+        if i % 3 == 0:
+            print(end="Fizz")
+        if i % 5 == 0:
+            print(end="Buzz")
+        if i % 3 and i % 5:
+            print(end=str(i))
+        print(end=" ")
+    print()
