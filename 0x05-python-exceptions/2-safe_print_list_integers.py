8ba5bd3037b7c5ee650bd2c3a4e8847e3b5e22c2
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-09-30 22:10:42 -0400

    remove finally printing

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
-    try:
-        for i in range(0, x):
-            try:
-                print(end="{:d}".format(my_list[i]))
-                printed += 1
-            except (ValueError, TypeError):
-                pass
-    finally:
-        print()
+    # try:
+    for i in range(0, x):
+        try:
+            print(end="{:d}".format(my_list[i]))
+            printed += 1
+        except (ValueError, TypeError):
+            pass
+    # finally:
+    print()
     return printed
