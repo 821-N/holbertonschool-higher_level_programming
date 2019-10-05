293ae7a897bd9c6ba0f5f93cc2ab129f88bd4ec8
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-10-05 02:23:14 -0400

    remove size 0 extra newline

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,12 +5,10 @@
 
 
 def print_square(size):
-    """ print a square with #s with an extra newline if size is 0 """
+    """ print a square with #s """
     if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise TypeError("size must be >= 0")
     for i in range(size):
         print("#"*size)
-    if size == 0:
-        print()
