781ec808d4647526d28f7869533dd426534f8e14
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-10-04 21:38:03 -0400

    add 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Print Square
+"""
+
+
+def print_square(size):
+    """ print a square with #s with an extra newline if size is 0 """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
+    for i in range(size):
+        print("#"*size)
+    if size == 0:
+        print()
