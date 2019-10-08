a897a58f23ea6f8d9835c74b542a5c93bbd274ba
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-10-07 20:39:46 -0400

    remvoe whitespace

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
-    
+
     @property
     def height(self):
         return self.__height
