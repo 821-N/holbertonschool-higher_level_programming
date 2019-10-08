c155a9232e96aa2a4b4fa84fe56df5368f058e24
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-10-08 14:31:16 -0400

    oops I didn't see this one

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Say My Name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ Print "My name is <first> <last>" """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
