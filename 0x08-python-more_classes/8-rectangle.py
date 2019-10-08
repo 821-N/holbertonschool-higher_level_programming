34ff1355322abbaead890c81c8c36c3656e311f0
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-10-08 14:23:01 -0400

    fix typo rect1->rect_1
    darn just when I stopped doing tests...

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,9 +75,9 @@ class Rectangle:
         print("Bye rectangle...")  # :(
 
     def bigger_or_equal(rect_1, rect_2):
-        """ return the larger rectangle (by area), or rect1 if equal """
+        """ return the larger rectangle (by area), or rect_1 if equal """
         if type(rect_1) != Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) != Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        return rect_1 if rect1.area() >= rect2.area() else rect2
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
