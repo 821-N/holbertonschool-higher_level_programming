1a83e4993db9e6810396a27158b905c7a8008a0f
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-10-16 18:15:48 -0400

    fix style

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,9 +7,9 @@
 def read_lines(filename="", nb_lines=0):
     """ Print the first `nb_lines` lines of file `filename` """
     with open(filename) as f:
-        count = 0 
+        count = 0
         for line in f:  # why can't I write f[:nb_lines] ??
             count += 1
-            if count>nb_lines:
+            if count > nb_lines:
                 break
             print(end=line)
