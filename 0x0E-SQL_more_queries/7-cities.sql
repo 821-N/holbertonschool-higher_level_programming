22505113b54c01936d391e2a6c0594d6993820ff
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-11-27 20:19:50 -0500

    fix comma

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,9 +3,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
       id INT UNIQUE NOT NULL AUTO_INCREMENT,
-      state_id 
+      state_id INT,
       name VARCHAR(256) NOT NULL,
-	   PRIMARY KEY (id)
+	   PRIMARY KEY (id),
       FOREIGN KEY (state_id)
               REFERENCES states(id)
 );
