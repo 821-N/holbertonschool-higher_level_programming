527f26af13239d949446fbd5473fbe3c7e494af1
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-12-16 10:53:04 -0500

    PLEASE

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+"""
+    ligma balls
+"""
+
+
 import MySQLdb
 from sys import argv
 
-# ligma balls
 
-if __name__ != "__main__":
-    return
-conn = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3]
-)
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-for row in cur.fetchall():
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
