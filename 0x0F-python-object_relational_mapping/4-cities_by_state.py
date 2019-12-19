df373c23dfd731464b54fd19a866a65fd373b8fd
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-12-19 14:17:55 -0500

    add 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    list all cities and their states
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cur = conn.cursor()
+    cur.execute("""
+    SELECT city.id, city.name, state.name
+    FROM cities city
+    LEFT JOIN states state
+    ON state_id = state.id
+    ORDER BY id ASC
+    """)
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
