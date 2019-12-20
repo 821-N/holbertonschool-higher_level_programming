c9f280bbb9a55aaa26d66c33678bd01270feb4a7
821N <nathaniel.marofsky@gmail.com>
2019-12-19 22:35:48 -0500

    add 12 :)

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    11. Student to JSON
+"""
+
+
+class Student:
+    """ student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ init """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to dict """
+        if type(attrs) == list:
+            l = []
+            for name in attrs:
+                l += getattr(self, name)
+                return l
+        else:
+            return self.__dict__
