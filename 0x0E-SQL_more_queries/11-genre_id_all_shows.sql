360fecdd5ab445814d2de527d9c9ed46b83efc0f
821N <nathaniel.marofsky@gmail.com>
2019-12-21 06:17:17 -0500

    add 10 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- list genres for all shows
 SELECT show.title, genre.genre_id
 FROM tv_shows show
-INNER JOIN tv_show_genres genre
+LEFT JOIN tv_show_genres g
 ON show.id = genre.show_id
 ORDER BY show.title, genre.genre_id ASC;
