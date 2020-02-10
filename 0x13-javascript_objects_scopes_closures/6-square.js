ceba0391d9ea7482213995dd159907afeab10a25
821-N <nathaniel.marofsky@gmail.com>
2020-02-10 13:35:29 -0500

    add 6'

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* eslint-disable */
+
+class Square extends require("./5-square.js") {
+	charPrint(c = "X") {
+		for (let i=0; i<this.height; i++)
+			console.log(c.repeat(this.width));
+	}
+}
+
+module.exports = Square;
